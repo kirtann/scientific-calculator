0bf2687755d41753ede0f7c1306751f708c2d63e
kirtann <94744255+kirtann@users.noreply.github.com>
2024-05-22 16:55:14 +0530

    theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import { evaluate, round } from "mathjs";
+import { evaluate, random, round } from "mathjs";
 import { btnValues } from "./constants/button";
 
 function App() {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
+  const [theme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme((prev) => !prev);
+  };
 
   document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
@@ -100,6 +105,35 @@ function App() {
     return stack.length === 0;
   };
 
+  const calcuate = () => {
+    if (input === "") return;
+    let finalexpression = input;
+    while (finalexpression.includes("\u00D7")) {
+      finalexpression = finalexpression.replace("\u00D7", "*");
+    }
+    while (finalexpression.includes("\u00F7")) {
+      finalexpression = finalexpression.replace("\u00F7", "/");
+    }
+    let result = 0;
+    try {
+      if (!checkBracketBalanced(finalexpression)) {
+        const errorMessage = { message: "Brackets are not balanced!" };
+        throw errorMessage;
+      }
+      result = eval(finalexpression);
+    } catch (error) {
+      console.log(error);
+      result =
+        error.message === "Brackets are not balanced!"
+          ? "Brackets not balanced!"
+          : "Invalid Input!!";
+    }
+
+    isNaN(result)
+      ? setOutput(result.toString())
+      : setOutput(round(result, 3).toString());
+  };
+
   const inputHandler = (val) => {
     if (output === "Invalid Input!!") return;
 
@@ -177,32 +211,20 @@ function App() {
         setInput(input + Math.PI.toFixed(2));
         break;
       case "=":
-        if (input === "") return;
-        let finalexpression = input;
-        while (finalexpression.includes("\u00D7")) {
-          finalexpression = finalexpression.replace("\u00D7", "*");
-        }
-        while (finalexpression.includes("\u00F7")) {
-          finalexpression = finalexpression.replace("\u00F7", "/");
-        }
-        let result = 0;
-        try {
-          if (!checkBracketBalanced(finalexpression)) {
-            const errorMessage = { message: "Brackets are not balanced!" };
-            throw errorMessage;
-          }
-          result = eval(finalexpression);
-        } catch (error) {
-          console.log(error);
-          result =
-            error.message === "Brackets are not balanced!"
-              ? "Brackets not balanced!"
-              : "Invalid Input!!";
-        }
-
-        isNaN(result)
-          ? setOutput(result.toString())
-          : setOutput(round(result, 3).toString());
+        calcuate();
+        break;
+      case "Rad":
+        break;
+      case "mc":
+        break;
+      case "m+":
+        break;
+      case "m-":
+        break;
+      case "mr":
+        break;
+      case "Rand":
+        setInput(input + random());
         break;
       default:
         inputHandler(val);
@@ -211,15 +233,65 @@ function App() {
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className=" h-[70vh] w-[70vw] bg-gray-700">
+    <div
+      className={`h-screen w-screen flex justify-center items-center ${
+        theme ? " border border-black" : "bg-gray-900"
+      }`}
+    >
+      <div className="absolute right-0 top-0 mt-6 mr-6">
+        <label className="inline-flex items-center cursor-pointer">
+          <span
+            className={`me-3 text-sm font-medium ${
+              theme ? "text-gray-900" : "text-white"
+            }`}
+          >
+            light
+          </span>
+          <input
+            type="checkbox"
+            value=""
+            onChange={() => {
+              setTheme((prev) => !prev);
+            }}
+            className="sr-only peer"
+            checked={theme}
+          />
+          <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span
+            className={`ms-3 text-sm font-medium ${
+              theme ? "text-gray-900" : "text-white"
+            }`}
+          >
+            dark
+          </span>
+        </label>
+      </div>
+      <div
+        className={` h-[70vh] w-[70vw] ${
+          theme ? "bg-gray-700" : "bg-white border border-white"
+        }`}
+      >
         <div className="w-full h-[20%] flex justify-end items-center">
           {output === "" ? (
-            <h1 className=" text-white text-6xl  me-2">{input}</h1>
+            <h1
+              className={` ${
+                theme ? "text-white" : "text-black"
+              } text-6xl  me-2`}
+            >
+              {input}
+            </h1>
           ) : (
             <div className="flex flex-col justify-end items-end me-2">
-              <h1 className=" text-white text-2xl ">{input}</h1>
-              <h1 className=" text-white text-6xl ">{output}</h1>
+              <h1
+                className={` ${theme ? "text-white" : "text-black"} text-2xl `}
+              >
+                {input}
+              </h1>
+              <h1
+                className={` ${theme ? "text-white" : "text-black"} text-6xl `}
+              >
+                {output}
+              </h1>
             </div>
           )}
         </div>
@@ -231,9 +303,9 @@ function App() {
                 onClick={() => handleClick(value.val)}
                 className={`flex justify-center ${
                   value.val === "0" ? "col-span-2" : ""
-                } items-center ${
-                  value.col
-                } text-white text-xl cursor-pointer hover:bg-emerald-500`}
+                } items-center ${value.col} ${
+                  theme ? "text-white" : "text-black"
+                } text-xl cursor-pointer hover:bg-emerald-500`}
               >
                 {value.val}
               </div>
@@ -248,9 +320,9 @@ function App() {
                 <div
                   className={`flex justify-center ${
                     value.val === "0" ? "col-span-2" : ""
-                  } items-center ${
-                    value.col
-                  } text-white text-xl cursor-pointer hover:bg-emerald-500`}
+                  } items-center ${value.col} ${
+                    theme ? "text-white" : "text-black"
+                  } text-xl cursor-pointer hover:bg-emerald-500`}
                   key={index}
                 >
                   {value.val}
